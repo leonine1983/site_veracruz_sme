443141cf63cb041ed4add00902bec18689a00674
Rogerio da Silva <rogerleonino@gmail.com>
2025-02-12 15:51:49 -0300

    links

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome do Site')
+    url = models.URLField(verbose_name='URL do Site')
+    icone = models.ImageField(upload_to='icones/', null=True, blank=True, verbose_name='Ícone (PNG)')
+    descricao = models.TextField(null=True, blank=True, verbose_name='Descrição do Site')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Link'
+        verbose_name_plural = 'Links'
