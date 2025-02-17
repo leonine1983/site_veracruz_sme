6bf93c9622c884b1e8640789a506728436e4d7b6
Rogerio da Silva <rogerleonino@gmail.com>
2025-02-17 12:25:52 -0300

    atualiza

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,7 @@ app_name = 'painel'
 urlpatterns = [
     path('/login', loginCreated, name='login'),
     path('logout/', user_logout, name='logout'),
+    
     path('', PublicacaoListView.as_view(), name='publicacao_list'),
     path('criar/', PublicacaoCreateView.as_view(), name='publicacao_create'),
     path('<int:pk>/editar/', PublicacaoUpdateView.as_view(), name='publicacao_edit'),
